6d82ca3c5eab466611999be1b0f8d27dcd5d5c16
Akinkunmi Akinnitire <tetisimiak@gmail.com>
2026-01-03 19:42:41 +0100

    error

--- src/components/user/StatsContainer.tsx
+++ src/components/user/StatsContainer.tsx
@@ -15,7 +15,7 @@ const StatsContainer = (props: StatsContainerProps) => {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-2 mb-8">
       <StatsCard title="Total Repositories" count={totalRepos} />
       <StatsCard title="Followers" count={followers} />
-      <StatsCard title="Following" count={following} />\
+      <StatsCard title="Following" count={following} />
       <StatsCard title="Gists" count={gists} />
     </div>
   )
